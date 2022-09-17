3d6d850ba54c241306df8acbc582b40d199a01a3
Gabriel Fok <gabrielfok128@gmail.com>
2022-09-17 15:02:24 -0700

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "INSTALLING DOTNET FOR LINUX"
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
@@ -7,3 +8,4 @@ sudo apt-get update; \
 sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
 sudo apt-get install -y dotnet-sdk-6.0
+echo "FINISHED INSTALLING DOTNET"
